2edaedcbc12cc916ca84cc66d309e64ff3b6fe58
Mahmud <13203031@iubat.edu>
2021-03-08 23:42:13 +0600

    Added Two Files

---
+++ Reversing Integer Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Reversing_InegerNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Type Your Number");
+            int number = Convert.ToInt32(Console.ReadLine()); ;
+            int reverseNumber = 0;
+            while (number!=0)
+            { 
+                int lastDigit = Convert.ToInt32(number % 10);
+                number = Convert.ToInt32(number / 10);
+                reverseNumber = reverseNumber * 10 + lastDigit;
+            }
+            Console.WriteLine("Reverse Number is " + reverseNumber);
+
+
+
+
+
+        }
+    }
+}
---
+++ TwoSum/leetcode_TwoSum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int i, j;
+        for (i = 0; i < nums.Length; i++)
+        {
+            for (j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[2] { i, j };
+                }
+
+            }
+
+        }
+        return new int[2] { 0, 0 };
+    }
+     void Main(string[] args)
+    {
+        // Pass the array as an argument.
+        TwoSum int [2] = new int[2] { 2, 12 };
+
+        // Keep the console window open in debug mode.
+        System.Console.WriteLine("Press any key to exit.");
+        System.Console.ReadKey();
+
+       
+        
+    }
+}
